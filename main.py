0edf30c0c33f496f3cc89e9e256edfa11a5b8130
Valentina Rivano <valentinarivano@MacBook-Pro-1010.local>
2025-04-13 16:37:25 -0400

    Just created 2 line charts, 1 bar chart, and 1 pie chart

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from db_insert import insert_alpha_weekly_data
-from visualize import plot_price
+from visualize import (
+    plot_price,
+    plot_high_low_avg_comparison,
+    plot_avg_close_bar,
+    plot_volume_pie,
+    plot_multi_symbol_trend
+)
 
 if __name__ == "__main__":
     symbol = "AAPL"
+
     insert_alpha_weekly_data(symbol)
 
-    # Visualizations
     plot_price(symbol, "close", "Closing", "blue")
     plot_price(symbol, "open", "Opening", "green")
     plot_price(symbol, "high_low_avg", "High-Low Average", "purple")
+
+    plot_high_low_avg_comparison(symbol)
+    plot_avg_close_bar(symbol)
+    plot_volume_pie(symbol)
+
+    plot_multi_symbol_trend(["AAPL", "TSLA", "MSFT"])
